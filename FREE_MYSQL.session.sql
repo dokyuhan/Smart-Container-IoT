badc45c7df95335bfde5037bee6787af35e48b74
dokyuhan <70351181+dokyuhan@users.noreply.github.com>
2024-01-27 21:57:23 +0000

    Pending changes exported from your codespace

--- FREE_MYSQL.session.sql
+++ FREE_MYSQL.session.sql
@@ -1,11 +1,11 @@
 USE `sql3678877`;
 
-DROP TABLE IF EXISTS `dht_sensor_data`;
+DROP TABLE IF EXISTS `sensor_bote`;
 
-CREATE TABLE `dht_sensor_data` (
+CREATE TABLE `sensor_bote` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `humidity` float DEFAULT NULL,
-  `temperature` float DEFAULT NULL,
+  `capacidad` float DEFAULT NULL,
+  `porcentaje` int DEFAULT NULL,
   `date_time` VARCHAR(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3;
+) ENGINE=InnoDB AUTO_INCREMENT=1;
--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, jsonify, render_template, request
 from twilio.rest import Client
 import mysql.connector
+from datetime import datetime
+import pytz
+import matplotlib.pyplot as plt
+from io import BytesIO
+import base64
 
 #credenciales para el acceso al cliente 
 account_sid = 'AC19c841aa09fb5083ae45d96139fa8db1'
@@ -9,6 +14,7 @@ client = Client(account_sid, auth_token)
 
 app = Flask(__name__)
 
+
 def createConnection(user_name, database_name, user_password, host, port):  #crear una conexion con los parametros de la base de datos
     cnx = mysql.connector.connect(
         user=user_name, database=database_name, password=user_password, host=host, port=port)
@@ -16,51 +22,120 @@ def createConnection(user_name, database_name, user_password, host, port):  #cre
     return (cnx, cursor)
 
 #esta ruta solamente recibimos datos del emulador virtual
-@app.route('/sensor_data', methods=['POST']) #Pregunta para examen, cuantos metodos y cuales (DELETE, GET, POST, PUT)
-def receive_sensor_data():
-
+@app.route('/sensor_bote', methods=['POST'])
+def receive_sensor_bote():
+    
     if request.headers['Content-Type'] == 'application/json':
 
-        data = request.json
+        capacidad = request.json
+        porcentaje = request.json
 
-        humidity = str(data.get('humidity'))
-        temperature = str(data.get('temperature'))
-        date_time = data.get('date_time')
+        mexico_tz = pytz.timezone('America/Mexico_City')
+        now_utc = datetime.utcnow()
+        now_mexico = now_utc.replace(tzinfo=pytz.utc).astimezone(mexico_tz)
+        date_time = now_mexico.strftime("%m/%d/%Y, %H:%M:%S")
 
-        print(humidity)
-        print(temperature)
+        capacidad = int(capacidad.get('capacidad'))
+        porcentaje = int(porcentaje.get('porcentaje'))
+
+        print(capacidad)
+        print(porcentaje)
         print(date_time)
-        message = client.messages.create(
-        from_='whatsapp:+14155238886',
-        body='Humedad: '+humidity+" temperatura:"+temperature,
-        to='whatsapp:+5215585735929'
-        )
-        print(message.sid)
+
+        if porcentaje in (95, 96, 97, 98, 99, 100):
+            message = client.messages.create(
+            from_='whatsapp:+14155238886',
+            body='El bote esta lleno' ' ,Capacidad: '+ str(capacidad)+ ' ,Porcentaje: ' + str(porcentaje)+' % \nFecha y hora: ' +date_time,
+            to='whatsapp:+5215585735929'
+            )
+            print(message.sid)
 
         cnx, cursor = createConnection('sql3678877', 'sql3678877', 'gRuTQXqsSI', 'sql3.freemysqlhosting.net', '3306')   #conexion a la base de datos
 
-        add_data = ("INSERT INTO dht_sensor_data (humidity, temperature, date_time) VALUES (%s, %s, %s)")  #inserta los queries a la base de datos
+        add_data = ("INSERT INTO sensor_bote (capacidad, porcentaje, date_time) VALUES (%s, %s, %s)")  #inserta los queries a la base de datos
         
-        cursor.execute(add_data, (humidity, temperature, date_time))  #ejecuta los queries hola
+        cursor.execute(add_data, (capacidad, porcentaje, date_time))  #ejecuta los queries
         cnx.commit()  #hace commit a la base de datos (guarda los datos a la base de datos)
         cursor.close()  #cierra el cursor
-        cnx.close()  #cierra la conexion
-
-        return 'Data received successfully.', 200
+        cnx.close()  #cierra la conexion 
+        
+        response_data = {'message': 'Data received successfully'}
+        return jsonify(response_data), 200
     else:
-        return 'Invalid content type. Expected application/json.', 0
+        response_data = {'status': 'error', 'message': 'Invalid content type. Expected application/json.'}
+        return jsonify(response_data), 0
+    
+#crea las graficas en base a la base de datos SQL
+@app.route("/grafica", methods=['GET'])
+def graph():
+        # Create a connection to the database
+    cnx, cursor = createConnection('sql3678877', 'sql3678877', 'gRuTQXqsSI', 'sql3.freemysqlhosting.net', '3306')
+
+    # Query the database
+    query = ("SELECT * FROM sensor_bote")
+
+    # Execute the query
+    cursor.execute(query)
+
+    # Get the data
+    data = cursor.fetchall()   #fetchall() obtiene todos los datos de la base de datos
+
+    # Cierra la conexión
+    cursor.close()
+    cnx.close()
+
+    capacidades = [item[2] for item in data]
+    dates = [datetime.strptime(item[3], "%m/%d/%Y, %H:%M:%S").strftime("%H:%M") for item in data]
+
+    #date_time = [item[3] for item in data]
+
+    #Datos para la grafica de barras
+    plt.figure(figsize=(12, 4))
+    plt.plot(dates, capacidades, label = 'capacidad')
+    plt.xlabel('Time')
+    plt.ylabel('Capacidad (%)')
+    plt.title('Capacidad vs. Tiempo')
+    plt.legend()
+
+    # Guardar la gráfica en un archivo temporal
+    img = BytesIO()
+    plt.savefig(img, format='png')
+    img.seek(0)                                           
+    img_data = base64.b64encode(img.getvalue()).decode()   #decodifica la imagen a base 64
+
+    last_data = capacidades[-1]
+    empty_percentage = 100 - last_data
+
+    # Datos para la gráfica de pie
+    sizes = [last_data, empty_percentage]
+    labels = ['Lleno', 'Vacio']
+
+    # Plotting the Pie Chart
+    plt.figure(figsize=(8, 8))
+    plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90, colors=['#ff9999', '#66b3ff'])
+    plt.legend()
+
+    # Guardar la gráfica en un archivo temporal
+    img2 = BytesIO()
+    plt.savefig(img2, format='png')
+    img2.seek(0)                                           
+    img_data2 = base64.b64encode(img2.getvalue()).decode()   #decodifica la imagen a base 64
+
+    # Regresa los datos de la imagen
+    return img_data, img_data2
 
 #solamente muestra la pagina web
-@app.route("/")
-def hello_world():
-    message = client.messages.create(
-    from_='whatsapp:+14155238886',   #whatsapp hello
-    body='Servicio activado',
-    to='whatsapp:+5215585735929'
-    )
-    print(message.sid)
-    return render_template("index.html", title="Alumno", tamano="small" )
+@app.route("/", methods=['GET'])
+def contenedores():
+    return render_template('main.html',tamano = "small")
 
-@app.route("/cafeteria")
-def hello_world1():
-    return render_template("cafeteria.html", title="Cafeteria")
+@app.route("/contenedor_1", methods=['GET'])
+def contenedor_1():
+    #trae los datos de la imgane de la grafica
+    img_data, img_data2 = graph()
+    return render_template('Contenedor1.html', img_data2 = img_data2 ,tamano = "small")
+
+@app.route("/agregar", methods=['GET'])
+def agregar():
+    img_data, img_data2 = graph()
+    return render_template('agregar.html',img_data = img_data, tamano = "small")
